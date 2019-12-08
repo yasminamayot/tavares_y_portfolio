5f467e60da529e87c6db4edebe226c66100d940d
yasminamayot <yasminamayot@gmail.com>
2019-12-08 17:32:14 -0500

    added database credentials to heroku

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    host: "localhost",
-    port: 8889, // 8889 for mac
-    uname: "root",
-    pword: "root", // leave blank for windows
-    database: "db_portfolio"
+    host: "ec2-174-129-255-26.compute-1.amazonaws.com",
+    port: 5432, // 8889 for mac
+    uname: "srylqqeeekkmhb",
+    pword: "1e93aa7cb3b89954edd920493cb6c2bcface128881765345201a9f038e4766a0", // leave blank for windows
+    database: "dftcbj5fj191pa"
 }
 
 
